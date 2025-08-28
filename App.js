a4cca7867cf7a22e498be6fe0a5f7e720105df3e
Maluks101 <ST10496079@imconnect.edu.za>
2025-08-28 19:18:02 +0200

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Tiro Maluleke</Text>
+      <Text>Student Number: 10496079</Text>
       <StatusBar style="auto" />
     </View>
   );
